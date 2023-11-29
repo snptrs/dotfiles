3f218ce704bd8d06732d01917dd0c0f63f556c22
Sean Peters <sean@getcrisp.co.uk>
2023-11-29 07:45:10 +0000

    Add additional config files

--- dot_config/nvim/lua/sp/plugins/lualine.lua
+++ dot_config/nvim/lua/sp/plugins/lualine.lua
@@ -1,11 +1,10 @@
 return {
-  -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
+  event = 'ColorScheme',
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'onedark',
+      theme = 'rose-pine',
       component_separators = '|',
       section_separators = '',
     },
