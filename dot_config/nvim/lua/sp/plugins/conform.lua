fdb7406235d95359077a8e3bd75637f910671b8d
Sean Peters <sean@snptrs.dev>
2023-12-01 12:48:30 +0000

    Update Conform config for PHP

--- dot_config/nvim/lua/sp/plugins/conform.lua
+++ dot_config/nvim/lua/sp/plugins/conform.lua
@@ -1,62 +1,58 @@
 return {
 	"stevearc/conform.nvim",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				svelte = { { "prettierd", "prettier" } },
-				css = { { "prettierd", "prettier" } },
-				scss = { { "prettierd", "prettier" } },
-				html = { { "prettierd", "prettier" } },
-				json = { { "prettierd", "prettier" } },
-				yaml = { { "prettierd", "prettier" } },
-				markdown = { { "prettierd", "prettier" } },
-				graphql = { { "prettierd", "prettier" } },
-				lua = { "stylua" },
-				python = { "isort", "black" },
-			},
-			format_on_save = {
-				function(bufnr)
-					-- Disable with a global or buffer-local variable
-					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-						return
-					end
-					return { timeout_ms = 500, lsp_fallback = true }
-				end,
-			},
-		})
-
-		vim.api.nvim_create_user_command("FormatDisable", function(args)
-			if args.bang then
-				-- FormatDisable! will disable formatting just for this buffer
-				vim.b.disable_autoformat = true
-			else
-				vim.g.disable_autoformat = true
+	event = { "BufWritePre" }, -- to disable, comment this out
+	cmd = { "ConformInfo" },
+	notify_on_error = true,
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>df",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+			svelte = { { "prettierd", "prettier" } },
+			css = { { "prettierd", "prettier" } },
+			scss = { { "prettierd", "prettier" } },
+			html = { { "prettierd", "prettier" } },
+			json = { { "prettierd", "prettier" } },
+			yaml = { { "prettierd", "prettier" } },
+			markdown = { { "prettierd", "prettier" } },
+			graphql = { { "prettierd", "prettier" } },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			php = { "phpcbf" },
+		},
+		format_on_save = function(bufnr)
+			if vim.bo[bufnr].filetype == "php" then
+				return
 			end
-		end, {
-			desc = "Disable autoformat-on-save",
-			bang = true,
-		})
-		vim.api.nvim_create_user_command("FormatEnable", function()
-			vim.b.disable_autoformat = false
-			vim.g.disable_autoformat = false
-		end, {
-			desc = "Re-enable autoformat-on-save",
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
+			return { timeout_ms = 500, lsp_fallback = true }
+		end,
+		format_after_save = function(bufnr)
+			if not vim.bo[bufnr].filetype == "php" then
+				return
+			end
+			return { lsp_fallback = true }
+		end,
+		-- Customize formatters
+		formatters = {
+			phpcbf = {
+				prepend_args = { "--standard=PSR12", "--extensions=php" },
+			},
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
 }
