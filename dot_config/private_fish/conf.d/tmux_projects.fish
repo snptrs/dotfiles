74b584a46b78171f4a0609e03c606e26184fbe5f
Sean Peters <sean@snptrs.dev>
2023-12-04 20:12:33 +0000

    Add .config/fish/conf.d/tmux_projects.fish

---
+++ dot_config/private_fish/conf.d/tmux_projects.fish
@@ -0,0 +1,12 @@
+function tmpj
+
+set session (fd -d 1 -t d --hidden . ~/Code/Projects ~/Code/Projects/ipecs-connect 2>/dev/null | fzf)
+set session_name (basename "$session")
+
+# if not tmux has-session -t "$session_name"
+  tmux new-session -A -s "$session_name" -c "$session"
+# end
+
+tmux switch-client -t "$session_name"
+
+end
