5f62127ec5a2b8d15bb9ebf802c8f668da73ae1b
Sean Peters <sean@snptrs.dev>
2023-12-09 22:00:10 +0000

    Updates

--- dot_config/nvim/lua/config/opts.lua
+++ dot_config/nvim/lua/config/opts.lua
@@ -1,27 +1,27 @@
 local options = {
-	sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions",
-	hlsearch = true,
-	number = true,
-	scrolloff = 4,
-	relativenumber = true,
-	expandtab = true,
-	shiftwidth = 2,
-	softtabstop = 2,
-	tabstop = 4,
-	mouse = "a", -- Enable mouse mode
-	guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20-blinkon250-blinkwait700-blinkoff400,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
-	cursorline = true,
-	colorcolumn = "80",
+  sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions',
+  hlsearch = true,
+  number = true,
+  scrolloff = 4,
+  relativenumber = true,
+  expandtab = true,
+  shiftwidth = 2,
+  softtabstop = 2,
+  tabstop = 4,
+  mouse = 'a', -- Enable mouse mode
+  guicursor = 'n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20-blinkon250-blinkwait700-blinkoff400,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor',
+  cursorline = true,
+  colorcolumn = '80',
 }
 
 local globals = {
-	loaded_netrw = 1,
-	loaded_netrwPlugin = 1,
+  loaded_netrw = 1,
+  loaded_netrwPlugin = 1,
 }
 
 -- Sync clipboard between OS and Neovim.
 --  See `:help 'clipboard'`
-vim.o.clipboard = "unnamedplus"
+vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -34,32 +34,34 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
+vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = "menu,menuone,noselect"
+vim.o.completeopt = 'menu,menuone,noselect'
 
 vim.o.termguicolors = true
 
+vim.opt.fillchars:append { diff = '╱' }
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
 })
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 for k, v in pairs(globals) do
-	vim.g[k] = v
+  vim.g[k] = v
 end
--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -8,5 +8,21 @@ return {
       component_separators = '|',
       section_separators = '',
     },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { { 'filename', path = 4 } },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {},
+    },
   },
 }
