9d6f3fdd372e46dbe2fea50c8c60662b66b69f1b
Sean Peters <sean@snptrs.dev>
2023-12-12 12:25:02 +0000

    Update rose pine

--- dot_config/nvim/lua/plugins/rose-pine.lua
+++ dot_config/nvim/lua/plugins/rose-pine.lua
@@ -3,6 +3,14 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require('rose-pine').setup {
+      disable_italics = false,
+      bold_vert_split = false,
+      dim_nc_background = true,
+      groups = {
+        punctuation = 'subtle',
+      },
+    }
     vim.cmd 'colorscheme rose-pine'
   end,
 }
