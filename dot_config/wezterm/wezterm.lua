de78dc3d1322db1340478d928213c7d5da2a59c5
Sean Peters <sean@snptrs.dev>
2024-01-28 13:56:30 +0000

    Update wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -18,6 +18,7 @@ end
 -- For example, changing the color scheme:
 -- config.color_scheme = "rose-pine"
 config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+config.set_environment_variables = { VTE_VERSION = "6003" }
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.font_size = 13.5
 config.line_height = 1.2
