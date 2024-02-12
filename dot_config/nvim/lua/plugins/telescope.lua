fe6a3fc5b003808b48dd1e71b9623dd79989a9a4
Sean Peters <sean@snptrs.dev>
2024-02-12 20:37:00 +0000

    Updates

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -170,10 +170,16 @@ return {
     vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
     vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
     vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+    vim.keymap.set(
+      'n',
+      '<leader>fo',
+      ":lua require('telescope.builtin').live_grep({ grep_open_files = true })<cr>",
+      { silent = true, desc = '[F]ind by grep in [O]pen files' }
+    )
     vim.keymap.set('n', '<leader>fG', ':LiveGrepGitRoot<cr>', { desc = '[F]ind by [G]rep on Git Root' })
     vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
     vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = '[F]ind [R]esume' })
     vim.keymap.set('n', '<leader>fm', ':Telescope harpoon marks<cr>', { silent = true, desc = '[F]ind Harpoon [M]arks' })
-    vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<cr>', { silent = true, desc = '[F]ind [B]rowser at current path' })
+    vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<cr>', { silent = true, desc = '[F]ile [B]rowser at current path' })
   end,
 }
