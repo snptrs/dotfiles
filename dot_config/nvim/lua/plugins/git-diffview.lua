c0e84eba4e1a99b8827a002dca2ac3fa17452fa0
Sean Peters <sean@snptrs.dev>
2024-02-17 16:16:11 +0000

    Updates

--- dot_config/nvim/lua/plugins/git-diffview.lua
+++ dot_config/nvim/lua/plugins/git-diffview.lua
@@ -2,6 +2,11 @@ return {
   'sindrets/diffview.nvim',
   opts = {
     enhanced_diff_hl = true,
+    view = {
+      merge_tool = {
+        layout = 'diff3_mixed',
+      },
+    },
   },
   vim.keymap.set('n', '<leader>gdo', ':DiffviewOpen<CR>', { desc = '[G]it [D]iffview [O]pen' }),
   vim.keymap.set('n', '<leader>gdc', ':DiffviewClose<CR>', { desc = '[G]it [D]iffview [C]lose' }),
