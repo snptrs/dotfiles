e7899053296e77540d7654c3ab95cf26eba21656
Sean Peters <sean@snptrs.dev>
2024-02-17 16:16:11 +0000

    Updates

--- dot_config/nvim/lua/plugins/comment.lua
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -1,3 +1,23 @@
 return {
-  'numToStr/Comment.nvim', opts = {}
+  {
+    'joosepalviste/nvim-ts-context-commentstring',
+    opts = {
+      enable_autocmd = false,
+    },
+    config = {
+      function()
+        vim.g.skip_ts_context_commentstring_module = true
+      end,
+    },
+  },
+  {
+    'numtostr/comment.nvim',
+    dependencies = { 'joosepalviste/nvim-ts-context-commentstring' },
+    config = function()
+      local prehook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+      require('comment').setup {
+        pre_hook = prehook,
+      }
+    end,
+  },
 }
