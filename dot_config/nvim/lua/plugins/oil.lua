08020aba05ecbc7592731035dadc1fcf87bf98b4
Sean Peters <sean@getcrisp.co.uk>
2024-03-06 09:38:32 +0000

    Change Oil config

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,7 @@ return {
   'stevearc/oil.nvim',
   enabled = true,
   opts = {
+    skip_confirm_for_simple_edits = true,
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
   },
   -- Optional dependencies
