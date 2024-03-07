a51242aa6d6893e77e065684a48c9e3e1d95c3f1
Sean Peters <sean@snptrs.dev>
2024-03-07 07:48:03 +0000

    Update Lualine to use GitSigns data

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,47 +1,59 @@
 return {
   'nvim-lualine/lualine.nvim',
   event = 'ColorScheme',
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'rose-pine',
-      component_separators = '|',
-      section_separators = '',
-    },
-    sections = {
-      lualine_a = {
-        'mode',
-        function()
-          local reg = vim.fn.reg_recording()
-          if reg == '' then
-            return ''
-          end -- not recording
-          return 'recording @' .. reg
-        end,
-        function()
-          local statusline = require 'arrow.statusline'
-          return statusline.text_for_statusline_with_icons()
-        end,
+  config = function()
+    local function diff_source()
+      local gitsigns = vim.b.gitsigns_status_dict
+      if gitsigns then
+        return {
+          added = gitsigns.added,
+          modified = gitsigns.changed,
+          removed = gitsigns.removed,
+        }
+      end
+    end
+
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        theme = 'rose-pine',
+        component_separators = '|',
+        section_separators = '',
       },
-      lualine_b = {
-        'branch',
-        'diff',
-        'diagnostics',
+      sections = {
+        lualine_a = {
+          'mode',
+          function()
+            local reg = vim.fn.reg_recording()
+            if reg == '' then
+              return ''
+            end -- not recording
+            return 'recording @' .. reg
+          end,
+          function()
+            local statusline = require 'arrow.statusline'
+            return statusline.text_for_statusline_with_icons()
+          end,
+        },
+        lualine_b = {
+          { 'b:gitsigns_head', icon = '' },
+          { 'diff', source = diff_source },
+        },
+        lualine_c = {
+          { 'filename', path = 1 },
+        },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
       },
-      lualine_c = {
-        { 'filename', path = 1 },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
       },
-      lualine_x = { 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { 'filename' },
-      lualine_x = { 'location' },
-      lualine_y = {},
-      lualine_z = {},
-    },
-  },
+    }
+  end,
 }
