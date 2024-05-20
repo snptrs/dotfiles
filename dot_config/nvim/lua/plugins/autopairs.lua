0ec178102918a3c40e9ad9f17384bc5b25f10bd1
Sean Peters <sean@snptrs.dev>
2024-05-20 20:28:20 +0100

    Updates

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -2,7 +2,17 @@ return {
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    opts = {}, -- this is equalent to setup({}) function
+    config = true,
   },
-  { 'windwp/nvim-ts-autotag', opts = {} },
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {
+      opts = {
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+      },
+    },
+  },
 }
