1ef361420ccc2bbf09453a2e89cb5b020be5b4f1
Sean Peters <sean@snptrs.dev>
2024-07-15 13:07:28 +0100

    Update WhichKey config

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -4,15 +4,24 @@ return {
   config = function()
     -- document existing key chains
     require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>gd'] = { name = '[D]iff', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>x'] = { name = 'Trouble', _ = 'which_key_ignore' },
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+      { '<leader>g', group = '[G]it' },
+      { '<leader>g_', hidden = true },
+      { '<leader>gd', group = '[D]iff' },
+      -- { '<leader>gd_', hidden = true },
+      { '<leader>h', group = 'More git' },
+      -- { '<leader>h_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
+      { '<leader>x', group = 'Trouble' },
+      { '<leader>x_', hidden = true },
     }
   end,
 }
