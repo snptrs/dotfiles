33b29ee2147315d2ff8446711507f9e991dc0fa4
Sean Peters <sean@snptrs.dev>
2024-07-15 13:06:50 +0100

    Update Persistence config for new version

--- dot_config/nvim/lua/plugins/persistence.lua
+++ dot_config/nvim/lua/plugins/persistence.lua
@@ -2,11 +2,24 @@ return {
   'folke/persistence.nvim',
   event = 'BufReadPre', -- this will only start session saving when an actual file was opened
   opts = {
-    -- restore the session for the current directory
-    vim.api.nvim_set_keymap('n', '<leader>ss', [[<cmd>lua require("persistence").load()<cr>]], { desc = 'Restore current directory session' }),
-    --restore the last session
-    vim.api.nvim_set_keymap('n', '<leader>sl', [[<cmd>lua require("persistence").load({ last = true })<cr>]], { desc = 'Restore last session' }),
+    -- load the session for the current directory
+    vim.keymap.set('n', '<leader>ss', function()
+      require('persistence').load()
+    end, { desc = 'Restore current directory session' }),
+
+    -- select a session to load
+    vim.keymap.set('n', '<leader>sS', function()
+      require('persistence').select()
+    end, { desc = 'Select a session to load' }),
+
+    -- load the last session
+    vim.keymap.set('n', '<leader>sl', function()
+      require('persistence').load { last = true }
+    end, { desc = 'Load the last session' }),
+
     -- stop Persistence => session won't be saved on exit
-    vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require("persistence").stop()<cr>]], { desc = "Disable persistence (don't save session)" }),
+    vim.keymap.set('n', '<leader>sd', function()
+      require('persistence').stop()
+    end, { desc = "Disable persistence (don't save session)" }),
   },
 }
