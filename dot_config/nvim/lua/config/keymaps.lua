5623dc054b10d5e1dbb72d65e22f6220a35370aa
Sean Peters <sean@snptrs.dev>
2024-07-15 13:04:59 +0100

    Update keymaps and opts

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -23,7 +23,7 @@ vim.keymap.set('n', '<leader>\\', function()
 end, { desc = 'Tree focus', unique = true })
 
 -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {})
-map('n', 'x', '"xx', { desc = 'Delete single characters to the x register', unique = true })
+-- map('n', 'x', '"xx', { desc = 'Delete single characters to the x register', unique = true })
 map('n', 'c', '"cc', { desc = 'Yank change text to the c register', unique = true })
 
 -- map({ 'n', 'i' }, '<C-M-l>', ':nohl<cr>', { desc = 'Search highlighting off', silent = true, unique = true })
