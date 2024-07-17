c41d3d7b2477273264d0babd67dba2aa2e2e7691
Sean Peters <sean@snptrs.dev>
2024-07-17 16:42:52 +0100

    Update WhichKey to use new style mappings

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,27 +1,16 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  config = function()
-    -- document existing key chains
-    require('which-key').register {
-      { '<leader>c', group = '[C]ode' },
-      { '<leader>c_', hidden = true },
-      { '<leader>d', group = '[D]ocument' },
-      { '<leader>d_', hidden = true },
-      { '<leader>g', group = '[G]it' },
-      { '<leader>g_', hidden = true },
-      { '<leader>gd', group = '[D]iff' },
-      -- { '<leader>gd_', hidden = true },
+  opts = {
+    spec = {
+      { '<leader>c', group = 'Code' },
+      { '<leader>d', group = 'Document' },
+      { '<leader>g', group = 'Git' },
+      { '<leader>gd', group = 'Diff' },
       { '<leader>h', group = 'More git' },
-      -- { '<leader>h_', hidden = true },
-      { '<leader>r', group = '[R]ename' },
-      { '<leader>r_', hidden = true },
-      { '<leader>s', group = '[S]earch' },
-      { '<leader>s_', hidden = true },
-      { '<leader>w', group = '[W]orkspace' },
-      { '<leader>w_', hidden = true },
+      { '<leader>s', group = 'Session' },
+      { '<leader>w', group = 'Workspace' },
       { '<leader>x', group = 'Trouble' },
-      { '<leader>x_', hidden = true },
-    }
-  end,
+    },
+  },
 }
