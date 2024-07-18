d6c4f2ab9fbc5d6a42903927935d9fec132d42c1
Sean Peters <sean@snptrs.dev>
2024-07-18 21:07:57 +0100

    Update Noice docs formatting

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -22,6 +22,14 @@ return {
         ['vim.lsp.util.stylize_markdown'] = true,
         ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
       },
+      documentation = {
+        opts = { -- Move docs and signature help up so they don't overlap CMP popup
+          anchor = 'SW',
+          position = {
+            row = 1,
+          },
+        },
+      },
     },
     -- you can enable a preset for easier configuration
     presets = {
