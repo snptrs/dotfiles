45e261599642ba027ac7bcaa1db862adadd2d06e
Sean Peters <sean@snptrs.dev>
2024-08-12 21:32:55 +0100

    Update diffview

--- dot_config/nvim/lua/plugins/git-diffview.lua
+++ dot_config/nvim/lua/plugins/git-diffview.lua
@@ -13,4 +13,5 @@ return {
   vim.keymap.set('n', '<leader>gdp', ':DiffviewOpen<CR>', { desc = 'Git Diffview previous commit (HEAD~)' }),
   vim.keymap.set('n', '<leader>gdh', ':DiffviewFileHistory %<CR>', { desc = 'Git Diffview current file history' }),
   vim.keymap.set('n', '<leader>gdH', ':DiffviewFileHistory<CR>', { desc = 'Git Diffview cwd file history' }),
+  vim.keymap.set('n', '<leader>gdb', ':DiffviewOpen origin/main...HEAD', { desc = 'Git Diffview against a commit' }),
 }
