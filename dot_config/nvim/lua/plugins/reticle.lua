1c607ecb50ddedc34f55dda339490c80f69e74ef
Sean Peters <sean@snptrs.dev>
2024-09-12 14:25:28 +0100

    Update Reticle path

--- dot_config/nvim/lua/plugins/reticle.lua
+++ dot_config/nvim/lua/plugins/reticle.lua
@@ -1,5 +1,5 @@
 return {
-  'tummetott/reticle.nvim',
+  'snptrs/reticle.nvim',
   event = 'VeryLazy', -- optionally lazy load the plugin
   opts = {
     -- add options here if you wish to override the default settings
