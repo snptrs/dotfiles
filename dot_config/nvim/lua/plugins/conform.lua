3e52a4f3ab27555ea062751a37816a7951e6adf1
Sean Peters <sean@snptrs.dev>
2025-01-11 13:20:56 +0000

    feat(tools): add format toggle commands and rgignore utility
    - Add FormatDisable and FormatEnable commands to control autoformatting
    - Create rgignore fish function for managing .rgignore files
    - Update ripgrep configuration and global gitignore

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -35,12 +35,20 @@ return {
       fish = { 'fish_indent' },
     },
     format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
       if vim.bo[bufnr].filetype == 'php' then
         return
       end
       return { timeout_ms = 1000, lsp_fallback = true }
     end,
     format_after_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
       if not vim.bo[bufnr].filetype == 'php' then
         return
       end
@@ -59,5 +67,23 @@ return {
   init = function()
     -- If you want the formatexpr, here is the place to set it
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
   end,
 }
