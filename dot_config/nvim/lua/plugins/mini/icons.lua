838a54b56da6354cb79eafbdbdec914ddf9e16c1
Sean Peters <sean@snptrs.dev>
2025-01-17 08:13:25 +0000

    Add custom mini icons

--- dot_config/nvim/lua/plugins/mini/icons.lua
+++ dot_config/nvim/lua/plugins/mini/icons.lua
@@ -1,6 +1,15 @@
 return {
   setup = function()
-    require('mini.icons').setup()
+    require('mini.icons').setup {
+      extension = {
+        -- Add icons for custom extension. This will also be used in
+        -- 'file' category for input like 'file.my.ext'.
+        ['tmpl'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['fish'] = { glyph = '', hl = 'MiniIconsPurple' },
+        ['conf'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['bak'] = { glyph = '', hl = 'MiniIconsOrange' },
+      },
+    }
 
     package.preload['nvim-web-devicons'] = function()
       require('mini.icons').mock_nvim_web_devicons()
