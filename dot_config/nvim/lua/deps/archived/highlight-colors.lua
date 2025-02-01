cd506098b8045931fa6b8119f03a6c8522cd2815
Sean Peters <sean@snptrs.dev>
2025-02-01 10:46:12 +0000

    chore: remove archived plugins and add comment
    - Remove unused highlight-colors plugin
    - Remove unused noice plugin configuration
    - Add comment in git-conflict setup

--- dot_config/nvim/lua/deps/archived/highlight-colors.lua
+++
@@ -1,9 +0,0 @@
-deps.later(function()
-  deps.add {
-    source = 'brenoprata10/nvim-highlight-colors',
-  }
-  require('nvim-highlight-colors').setup {
-    enable_named_colors = false,
-    exclude_filetypes = { 'lazy', 'minideps-confirm' },
-  }
-end)
