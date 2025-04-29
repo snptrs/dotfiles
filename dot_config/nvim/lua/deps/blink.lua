8309893209328fa6270b2bd2bc29b2134bc31f61
Sean Peters <sean@snptrs.dev>
2025-04-29 09:34:26 +0100

    Update Zellij and Blink configs

--- dot_config/nvim/lua/deps/blink.lua
+++ dot_config/nvim/lua/deps/blink.lua
@@ -2,7 +2,7 @@ deps.now(function()
   deps.add {
     source = 'Saghen/blink.cmp',
     depends = { 'rafamadriz/friendly-snippets' },
-    checkout = 'v1.0.0',
+    checkout = 'v1.1.1',
     monitor = 'main',
   }
 
