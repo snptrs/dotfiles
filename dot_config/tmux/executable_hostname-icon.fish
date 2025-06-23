4a2513858d6b17beb95fa9b9b6bb21d14cd7203e
Sean Peters <sean@snptrs.dev>
2025-06-23 09:13:10 +0100

    Move tmux hostname icon to script

---
+++ dot_config/tmux/executable_hostname-icon.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+# Get hostname, fallback if command fails
+set host (hostname -s 2>/dev/null; or echo "unknown")
+
+switch $host
+    case Seans-Mac
+        printf "󰇄 "
+    case Seans-MacBook-Air
+        printf " "
+    case unknown
+        printf "❓ "
+    case '*'
+        # Truncate long hostnames if needed
+        if test (string length $host) -gt 15
+            printf "%s… " (string sub -l 8 $host)
+        else
+            printf "%s " $host
+        end
+end
