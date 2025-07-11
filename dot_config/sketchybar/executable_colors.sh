39bc54f9df5f77c70e28589771612221f2ff53e3
Sean Peters <sean@snptrs.dev>
2025-07-11 15:23:50 +0100

    Update aerospace and sketchybar configs

---
+++ dot_config/sketchybar/executable_colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export WHITE=0xffffffff
+
+# export BAR_COLOR=0xff101314
+export BAR_COLOR=0xff000000
+export ITEM_BG_COLOR=0xff353c3f
+export ACCENT_COLOR=0xffffffff
--- dot_config/sketchybar/items/executable_aerospace_common.sh
+++ dot_config/sketchybar/items/executable_aerospace_common.sh
@@ -8,8 +8,9 @@ sketchybar --add item space.$1 left \
     background.color=0x44ffffff \
     background.corner_radius=5 \
     background.height=20 \
-    background.width=0 \
     background.drawing=off \
+    label.padding_left=-4 \
+    label.padding_right=8 \
     label="$1" \
     click_script="aerospace workspace $1" \
     script="$CONFIG_DIR/plugins/aerospace.sh $1"
